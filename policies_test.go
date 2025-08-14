7c417bbc9861ddba735cc4134d6ef1ea1e8a27fd
Alysson Ribeiro <15274059+sonalys@users.noreply.github.com>
2025-08-14 14:55:08 +0200

    increase policies test coverage

--- policies_test.go
+++ policies_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/sonalys/sanitize"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/net/html/atom"
 )
 
 func Test_SanitizeEmail(t *testing.T) {
@@ -19,4 +20,38 @@ func Test_SanitizeEmail(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, string(content), out.String())
+}
+
+func Test_BlockAttrs(t *testing.T) {
+	content := []byte(`<html><head></head><body style="color:red"></body></html>`)
+	out := bytes.NewBuffer(make([]byte, 0, 1024))
+	err := sanitize.HTML(bytes.NewReader(content), out,
+		sanitize.BlockAttrs("style"),
+	)
+	require.NoError(t, err)
+
+	require.Equal(t, "<html><head></head><body></body></html>", out.String())
+}
+
+func Test_BlockTags(t *testing.T) {
+	content := []byte(`<html><head></head><body><a/></body></html>`)
+	out := bytes.NewBuffer(make([]byte, 0, 1024))
+	err := sanitize.HTML(bytes.NewReader(content), out,
+		sanitize.BlockTags(atom.A),
+	)
+	require.NoError(t, err)
+
+	require.Equal(t, "<html><head></head><body></body></html>", out.String())
+}
+
+func Test_AllowTags(t *testing.T) {
+	content := []byte(`<html><head></head><body><a/></body></html>`)
+	out := bytes.NewBuffer(make([]byte, 0, 1024))
+	err := sanitize.HTML(bytes.NewReader(content), out,
+		sanitize.Blacklist(),
+		sanitize.AllowTags(atom.Html, atom.Body, atom.A),
+	)
+	require.NoError(t, err)
+
+	require.Equal(t, "<html><body><a></a></body></html>", out.String())
 }
